a549f61eaf9a96ca233cf6ebe76c75ad162ac2ee
irfanmuzamil <irfanullahchagharzai@gmail.com>
2023-12-07 22:30:13 +0500

    items

--- src/components/ShopingListScreen.js
+++ src/components/ShopingListScreen.js
@@ -6,8 +6,9 @@ const ShoppingListScreen = ({ navigation }) => {
   const products = [
     { id: 1, name: 'Product 1' },
     { id: 2, name: 'Product 2' },
+    { id: 3, name: 'Product 3' },
   ];
-  
+
   return (
     <View>
       <Text>Shopping List Screen</Text>
